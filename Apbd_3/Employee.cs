cb6187b15c3ab08a9928dc9dd7801de9b3bbba80
Mateusz Sinior <s33786@pjwstk.edu.pl>
2026-03-25 19:44:12 +0100

    Added new variables to class Employee.cs and Student.cs also with its extension of abstract class Person. Deleted interface IEquipment.cs.

--- Apbd_3/Employee.cs
+++ Apbd_3/Employee.cs
@@ -1,6 +1,15 @@
 ﻿namespace Apbd_3;
 
-public class Employee
+public class Employee : Person
 {
+    public string salary {get; set;}
+    public string department {get; set;}
+
+    public Employee(int id, string name, string surname, string userType, string salary, string department) : base(id,
+        name, surname, userType)
+    {
+        this.salary = salary;
+        this.department = department;
+    }
     
 }
--- Apbd_3/Student.cs
+++ Apbd_3/Student.cs
@@ -1,6 +1,14 @@
 ﻿namespace Apbd_3;
 
-public class Student
+public class Student : Person
 {
-    
+    public string indexNumber {get; set;}
+    public string fieldOfStudy {get; set;}
+
+    public Student(int id, string name, string surname, string userType, string indexNumber, string fieldOfStudy) :
+        base(id, name, surname, userType)
+    {
+        this.indexNumber = indexNumber;
+        this.fieldOfStudy = fieldOfStudy;
+    }
 }
