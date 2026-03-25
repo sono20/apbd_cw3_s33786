fcc2d834989ed9cf96091de0cba518d6b091be81
Mateusz Sinior <s33786@pjwstk.edu.pl>
2026-03-25 22:01:15 +0100

    Created demonstration of functionalities of this program

--- Apbd_3/Program.cs
+++ Apbd_3/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Apbd_3;
 
-Console.WriteLine("Hello, World!");
+var service = new RentalService();
+//11
+service.addEquipment(new Laptop("Hp", true, "excellent condition", "i7", "RTX 3060"));
+service.addEquipment(new Camera("Canon R5", true, "good condition", "Canon RF", 45f));
+service.addEquipment(new Projector("Epson EB", true, "good condition", 3000, 1.5f));
+//12
+var student = new Student("Mateusz","Kowal","Student", "s12411", "Computer science");
+var employee = new Employee("Magda","Stolarczyk","Employee", 5000m, "IT");
+service.addUser(student);
+service.addUser(employee);
+//13
+var laptop = new Laptop("Dell XPS", true, "good condition", "i7", "RTX 3060");
+service.addEquipment(laptop);
+var rental = service.rentEquipment(student, laptop, 7);
+Console.WriteLine($"Rented {laptop.name} to {student.name} {student.surname}");
+//14
+try
+{
+    service.setEquipmentUnavailable(laptop.id);
+    service.rentEquipment(student, laptop, 7);
+} catch (Exception ex) {
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+//15
+service.returnEquipment(rental.id);
+Console.WriteLine($"Returned {laptop.name} on time");
+//16
+var camera = new Camera("Canon R5", true, "good condition", "Canon RF" ,45f);
+service.addEquipment(camera);
+var rental2 = service.rentEquipment(student, camera, 7);
+Console.WriteLine($"Penalty for late return: {rental2.penalty} PLN");
+
+rental2.dueDate = DateTime.Now.AddDays(-3);
+service.returnEquipment(rental2.id);
+Console.WriteLine($"Penalty: {rental2.penalty}");
+//17
+service.generateReport();
