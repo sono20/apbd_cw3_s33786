2d003543e153790909252aa3b34e5059ff71e80c
Mateusz Sinior <s33786@pjwstk.edu.pl>
2026-03-25 19:25:38 +0100

    Made new variables to classes and changed IEquipment from interface to abstract class

--- Apbd_3/Equipment.cs
+++ Apbd_3/Equipment.cs
@@ -2,19 +2,21 @@
 
 namespace Apbd_3;
 
-public class Equipment : IEquipment
+public abstract class Equipment 
 {
+    private static int _nextId = 1;
+    
     public int id { get; set; }
     public string name { get; set; }
     public int availability { get; set; }
-    public string sameData {get; set;}
+    public string commonData {get; set;}
 
-    public Equipment(int id, string name, int availability, string sameData)
+    public Equipment(int id, string name, int availability, string commonData)
     {
-        id = id;
-        name = name;
-        availability = availability;
-        sameData = sameData;
+        this.id = _nextId++;
+        this.name = name;
+        this.availability = availability;
+        this.commonData = commonData;
         
     }
 
--- Apbd_3/Laptop.cs
+++ Apbd_3/Laptop.cs
@@ -1,7 +1,13 @@
 ﻿namespace Apbd_3;
 
-public class Laptop
+public class Laptop : Equipment
 {
-    private string _processor;
-    private string GPU;
+    public string processor {get; set;}
+    public string GPU {get; set;}
+
+    public Laptop(int id, string name, int availability, string sameData, string processor, string GPU) : base(id, name, availability, sameData)
+    {
+        this.processor = processor;
+        this.GPU = GPU;
+    }
 }
