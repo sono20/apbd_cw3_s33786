254b267d4c23681d77fd5d4cd992feca17b2e39a
Mateusz Sinior <s33786@pjwstk.edu.pl>
2026-03-25 19:45:42 +0100

    Changed Computer.cs

---
+++ Apbd_3/Computer.cs
@@ -0,0 +1,14 @@
+﻿namespace Apbd_3;
+
+public class Computer : Equipment
+{
+    public int ramGb { get; set; }
+    public int storageGb { get; set; }
+
+    public Computer(int id, string name, int availability, string commonData, int ramGb, int storageGb) : base(id, name,
+        availability, commonData)
+    {
+        this.ramGb = ramGb;
+        this.storageGb = storageGb;
+    }
+}
